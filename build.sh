7bc46d9bc8f1f67bb4f69a4857a94dec3ec4d04b
Steve Korshakov <steve@korshakov.com>
2024-01-17 13:54:06 -0800

    feat: add lj speech dataset, multi platform build

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 set -e
+rm -fr ./out/
 mkdir -p ./out/
 cd ./sources/
-go build -o ../out/ .
+
+# Building
+GOOS=linux GOARCH=amd64 go build -o ../out/linux-x64/ .
+GOOS=darwin GOARCH=amd64  go build -o ../out/darwin-x64/ .
+GOOS=darwin GOARCH=arm64  go build -o ../out/darwin-arm64/ .
+GOOS=windows GOARCH=amd64 go build -o ../out/windows-x64/ .
