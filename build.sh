5203844f19075887b78a14e8d71da532b1953627
Steve Korshakov <steve@korshakov.com>
2024-01-24 21:57:14 -0800

    ref: update build script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ mkdir -p ./out/
 cd ./sources/
 
 # Building
-GOOS=linux GOARCH=amd64 go build -o ../out/datasets-linux-x64 .
-GOOS=darwin GOARCH=amd64  go build -o ../out/datasets-darwin-x64 .
+GOOS=linux GOARCH=amd64 go build -o ../out/datasets-linux-amd64 .
+GOOS=darwin GOARCH=amd64  go build -o ../out/datasets-darwin-amd64 .
 GOOS=darwin GOARCH=arm64  go build -o ../out/datasets-darwin-arm64 .
-GOOS=windows GOARCH=amd64 go build -o ../out/datasets-windows-x64.exe .
+GOOS=windows GOARCH=amd64 go build -o ../out/datasets-windows.exe .
