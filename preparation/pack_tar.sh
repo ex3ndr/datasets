fe18b3e845aa73fbe115267adeb1a0714b4681a7
Steve Korshakov <steve@korshakov.com>
2024-06-20 11:23:06 -0700

    feat: add libriheavy dataset

---
+++ preparation/pack_tar.sh
@@ -0,0 +1,16 @@
+set -e
+NAME="$1"
+
+# Pack dataset
+mv dataset $NAME
+COPYFILE_DISABLE=1 tar -zcf $NAME.tar $NAME
+mv $NAME dataset
+
+# Checksums without filename
+sha1=`shasum -a 1 $NAME.tar | cut -d ' ' -f 1`
+sha256=`shasum -a 256 $NAME.tar | cut -d ' ' -f 1`
+md5=`md5 $NAME.tar | cut -d ' ' -f 4`
+
+echo "sha1: $sha1"
+echo "sha256: $sha256"
+echo "md5: $md5"
